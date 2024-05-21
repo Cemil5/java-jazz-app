fb40691ac4a8b4a1aac8f05f8c4aeeaae62c27d7
efeaydin <efekrmaydn@gmail.com>
2024-05-21 01:36:18 +0100

    Worked on US3, completed all fields, only role needs to config further. Issues listed on Trello.

--- src/main/java/com/cydeo/enums/Gender.java
+++ src/main/java/com/cydeo/enums/Gender.java
@@ -1,4 +1,12 @@
 package com.cydeo.enums;
 
 public enum Gender {
+    MALE("Male"), FEMALE("Female");
+    private String value;
+    Gender(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
 }
